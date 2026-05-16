38599e0ef57c507a2882570cbfa0ecf1dc805a24
Ed Page <eopage@gmail.com>
2026-05-16 22:32:42 +0200

    feat(cli): Add initial parser

--- src/bin/cargo-ship.rs
+++ src/bin/cargo-ship.rs
@@ -1,4 +1,5 @@
 use cargo_ship::error;
+use clap::Parser;
 
 fn main() {
     let res = run();
@@ -8,10 +9,34 @@ fn main() {
 fn run() -> Result<(), error::CliError> {
     env_logger::Builder::from_env("CARGO_LOG").init();
 
+    let CargoCli::Ship(_ship) = CargoCli::parse();
+
     Ok(())
 }
 
 fn exit(result: Result<(), error::CliError>) -> ! {
     let code = error::report(result);
     std::process::exit(code)
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "cargo")]
+#[command(bin_name = "cargo")]
+#[command(styles = clap_cargo::style::CLAP_STYLING)]
+pub enum CargoCli {
+    #[command(name = "ship")]
+    #[command(about, author, version)]
+    Ship(ShipCli),
+}
+
+/// Automated release for Rust crates
+#[derive(Debug, Clone, clap::Args)]
+pub struct ShipCli {
+    #[command(subcommand)]
+    pub ops: Ops,
+}
+
+#[derive(Clone, Debug, clap::Subcommand)]
+pub enum Ops {
+    Changes,
 }
